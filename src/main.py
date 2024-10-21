36353fac89834efd0bc6cf1e0e9ee33ff4866eba
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-10-21 20:25:56 +0300

    Update main.py
    изменение условий задачи

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 # Создать в репозиторие файл main.py
-# Вывести на экран путь к директории, где лежит файл main.py
+# Вывести на экран путь к директории, где лежит файл
 # Закоммитить, приложить скрин и коммит.
 
 import os
+from zipfile import ZipFile
 
 current_file = os.path.relpath(__file__)
 current_directory = os.path.dirname(current_file)
 
-print(current_file)
+PATH_FILE_ZIP = "D:\Учеба\-blitz-croco-words\src\croco-blitz-source.zip"
+
+
+def zip_f():
+    with ZipFile(PATH_FILE_ZIP, mode="r") as archive:
+        print("Список файлов в архиве: ")
+        for f in archive.namelist():
+            print(f)
+
+
+if __name__ == '__main__':
+    print(current_file)
+
+    zip_f()
