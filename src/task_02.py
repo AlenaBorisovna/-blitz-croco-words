a062052a1c901296bc93848d0589814da988a01b
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-10-30 20:54:43 +0300

    Update task_02.py
    выполнение задачи 24.10.30.1

--- src/task_02.py
+++ src/task_02.py
@@ -2,20 +2,38 @@
 # Сохраните из этой презентации только слова, всякие "Правила", "Тур 1" и другие объяснение не нужны.
 # Сохраните все слова в файл words.txt
 # Закомитьте его тоже.
-
+import re
 
 from pptx import Presentation
 
+from pathlib import Path
 
-def get_file():
+FILE_TXT: str = 'words.txt'
+
+
+def get_words_presentation():
     prs = Presentation("Osennyaya_igra_11.pptx")
-
+    words_list = list()
     for slide in prs.slides:
         for shape in slide.shapes:
             if not shape.has_text_frame:
                 continue
-            print(shape.text)
+            if ' ' in shape.text or ':' in shape.text or '-' in shape.text:
+                continue
+            words_list.append(shape.text)
+
+    return words_list
+
+
+def write_txt(filename: str, words: list[str]):
+    try:
+        with open(filename, 'w', encoding='utf-8') as file_text:
+            for word in words:
+                file_text.write(word + '\n')
+        print(f"Слова записаны в файл: {filename}")
+    except IOError as e:
+        print(f"error: {e}")
 
 
 if __name__ == '__main__':
-    get_file()
+    write_txt(FILE_TXT, get_words_presentation())
