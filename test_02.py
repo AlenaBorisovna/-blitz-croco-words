2e06766e3c085d7df1bda02e10d3c2b590cd0180
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-01 19:03:17 +0300

    Create test_02.py
    выполнение задачи
    #Напишите тест, который проверяет

---
+++ test_02.py
@@ -0,0 +1,12 @@
+# Напишите тест, который проверяет, что в строке находится всего одно слово.
+
+def is_valid(words: str) -> bool:
+    return words.isalpha()
+
+
+def test():
+    assert is_valid('') == False
+    assert is_valid(' ') == False
+    assert is_valid('0') == False
+    assert is_valid('.') == False
+    assert is_valid('слово') == True
