2d0fe06a665ee5a3b92be9e00f178f4ecd99af84
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-04 12:17:27 +0300

    Update main.py
    выполнение зад 1 от 01.11

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
-# Создать в репозиторие файл main.py
-# Вывести на экран путь к директории, где лежит файл
-# Закоммитить, приложить скрин и коммит.
+# Теперь рефакторим код.
+# Ракладываем его по уровням и задачам.
+# 1 уровень - читаем архив и собираем слова из каждого файла
+# 2 уровень (ниже) - читаем файл с презентацией и достаём из него слова.
+# Уровни лучше разнести по разным файлам.
+
 
 import os
 from zipfile import ZipFile
+from pathlib import Path
+from helpers import get_words_presentation, write_txt
+
+FILE_ZIP = 'croco-blitz-source.zip'
 
 current_file = os.path.relpath(__file__)
 current_directory = os.path.dirname(current_file)
@@ -18,7 +25,16 @@ def zip_f():
             print(f)
 
 
-if __name__ == '__main__':
-    print(current_file)
+def unzip_zip_file():
+    cur_folder = os.path.dirname(os.path.realpath(__file__))
+    words: set[str] = set()
 
-    zip_f()
+    p = Path(cur_folder) / FILE_ZIP
+
+    with ZipFile(p) as zip_ref:
+        for f in zip_ref.namelist():
+            words.update(get_words_presentation(zip_ref.open(f)))
+
+
+if __name__ == '__main__':
+    unzip_zip_file()
