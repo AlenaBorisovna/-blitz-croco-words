84e4dca38f039395d63329dbeb3366cd108d0979
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-06 20:05:40 +0300

    Update helpers.py
    редакт. кода к задаче 24,11,01,02

--- helpers.py
+++ helpers.py
@@ -8,20 +8,25 @@ FILE_TXT: str = 'words.txt'
 FILE_ZIP = 'croco-blitz-source.zip'
 
 
-def get_words_presentation(file: IO[bytes]) -> list[str]:
+def is_not_valid(text: str) -> bool:
+    return ' ' in text or ':' in text or '-' in text
+
+
+def get_words_presentation(file: IO[bytes]) -> set[str]:
     prs = Presentation(file)
-    words_list = list()
+    # words_list = list()
+    words_set = set()
     for slide in prs.slides:
         for shape in slide.shapes:
             if not shape.has_text_frame:
                 continue
-            if ' ' in shape.text or ':' in shape.text or '-' in shape.text:
+            if is_not_valid(shape.text):
                 continue
-            words_list.append(shape.text)
-    return words_list
+            words_set.add(shape.text)
+    return words_set
 
 
-def write_txt(filename: str, words: set[str]):
+def write_txt(filename: str, words: set[str] or str):
     try:
         with open(filename, 'w', encoding='utf-8') as file_text:
             for word in words:
