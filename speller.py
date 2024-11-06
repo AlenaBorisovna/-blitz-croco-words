6d3670d2395d2f82e35010e36e91bbef800bee08
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-06 21:04:56 +0300

    Update speller.py
    выполнение задачи 24.10.11.05

--- speller.py
+++ speller.py
@@ -2,12 +2,11 @@ from pyaspeller import YandexSpeller
 
 
 def speller_word():
-    spellers: YandexSpeller = YandexSpeller()
-    letters: str = 'Колакал малако'
-    fixes: str = spellers.spelled(letters)
+    spellers = YandexSpeller()
+    letters: list[str] = ['Колакал', 'Малако']
+    fixes = spellers.spelled(' '.join(letters))
     print(fixes)
 
 
 if __name__ == '__main__':
     speller_word()
-   
