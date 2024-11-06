93afc79c49cee0c35110b7071ca379882d386df8
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-06 20:05:45 +0300

    Update main.py
    редакт. кода к задаче 24,11,01,02

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-# Теперь рефакторим код.
-# Ракладываем его по уровням и задачам.
-# 1 уровень - читаем архив и собираем слова из каждого файла
-# 2 уровень (ниже) - читаем файл с презентацией и достаём из него слова.
-# Уровни лучше разнести по разным файлам.
+# Задача 2024.11.01.02
+#
+# В какой контейнер данных нужно собирать слова чтобы исключить дубликаты?
+# Поменяйте код так, чтобы это сделать.
+# Подумайте на каком уровне лучше это делать и почему.
+# Напишите сколько было слов всего из всех файлов, когда вы сохраняли их в список,
+# и сколько стало, когда вы исключили дубликаты.
 
 
 import os
@@ -15,18 +17,22 @@ FILE_ZIP = 'src/croco-blitz-source.zip'
 
 def unzip_zip_file():
     cur_folder = os.path.dirname(os.path.realpath(__file__))
-    words: set[str] = set()
+    words: list[str] = list()
 
     p = Path(cur_folder) / FILE_ZIP
 
     with ZipFile(p) as zip_ref:
         for f in zip_ref.namelist():
-            words.update(get_words_presentation(zip_ref.open(f)))
+            words.extend(get_words_presentation(zip_ref.open(f)))
 
-    write_txt("words.txt", words)
+    words_set: set = set(words)
+    write_txt("words.txt", words_set)
+
+    print(f"{len(words)} слова-дубликаты\n и {len(words_set)} без дубликатов")
 
 
 if __name__ == '__main__':
     unzip_zip_file()
 
-
+# 2174 слова-дубликаты
+#  и 1635 без дубликатов
