70d6d653228474899b5f11fcfca43c8c8f06f3ef
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-06 20:33:45 +0300

    Update speller.py
    выполнение задачи 10.11.04

--- speller.py
+++ speller.py
@@ -1,11 +1,13 @@
 from pyaspeller import YandexSpeller
 
+
 def speller_word():
     spellers: YandexSpeller = YandexSpeller()
-    letters: str = 'в субботу утромъ'
+    letters: str = 'Колакал малако'
     fixes: str = spellers.spelled(letters)
     print(fixes)
 
 
 if __name__ == '__main__':
     speller_word()
+   
