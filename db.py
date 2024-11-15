581a387f00e7135a1e3e94574743f23803780a1a
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-15 19:25:42 +0300

    Update db.py
    задача №7 и 8 от 15.11

--- db.py
+++ db.py
@@ -4,7 +4,9 @@ import sqlite3
 def main():
     con = sqlite3.connect('words.db')
     cur = con.cursor()
-    cur.execute('CREATE TABLE movie(title, year, score)')
+    cur.execute('CREATE TABLE IF NOT EXISTS movie(title, year, score)')
+    res = con.execute("SELECT name FROM sqlite_master")
+    print(res)
 
 
 if __name__ == '__main__':
