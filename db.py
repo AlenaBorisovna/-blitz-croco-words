be9962d174efde77af3527b5870d87c4774835d8
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-15 20:34:15 +0300

    Update db.py
    выполнение задачи №14 от 15.11

--- db.py
+++ db.py
@@ -5,8 +5,16 @@ def main():
     con = sqlite3.connect('words.db')
     cur = con.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS movie(title, year, score)')
+
+    data = [
+        ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+        ("Monty Python's The Meaning of Life", 1983, 7.5),
+        ("Monty Python's Life of Brian", 1979, 8.0),
+    ]
+    cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+    con.commit()
     res = con.execute("SELECT name FROM sqlite_master")
-    print(res)
+    print(res.fetchone)
 
 
 if __name__ == '__main__':
