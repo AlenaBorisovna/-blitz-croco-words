58edd0d4236e0d0f5e61d5cfd66f2e4ff0f9ad81
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-11-15 20:46:29 +0300

    Update db.py
    комментарии к коду

--- db.py
+++ db.py
@@ -2,19 +2,23 @@ import sqlite3
 
 
 def main():
+    # Устанавливаем соединение с базой данных SQLite
     con = sqlite3.connect('words.db')
+    # Создаем объект курсора для выполнения SQL-запросов
     cur = con.cursor()
+    # Создаем таблицу movie с колонками title, year и score
     cur.execute('CREATE TABLE IF NOT EXISTS movie(title, year, score)')
 
+    # Создаем список данных
     data = [
         ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
         ("Monty Python's The Meaning of Life", 1983, 7.5),
         ("Monty Python's Life of Brian", 1979, 8.0),
     ]
+    # Используем executemany для вставки всех данных за один запрос
     cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+    # Сохраняем изменения в базе данных
     con.commit()
-    res = con.execute("SELECT name FROM sqlite_master")
-    print(res.fetchone)
 
 
 if __name__ == '__main__':
